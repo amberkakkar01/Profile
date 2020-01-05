7da23e6a0273bc71a74b2cf9b1d0c966e400a924
Amber Kakkar <52875602+amberkakkar01@users.noreply.github.com>
2020-01-05 10:44:42 +0530

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,14 @@
+/*
+* @Author: Yash
+* @Date:   2017-06-22 10:45:00
+* @Last Modified by:   Amber
+* @Last Modified time: 2019-11-30 10:50:19
+*/
+
+$(function(){
+	$("#about-me").typed({
+		strings: ["Student at DIT University.","Member IEEE Club.","Computer Vision | Web Developer","Open Source Enthusiast","Interested in Database and Machine Learning", "Optimist | Sports |Techie.","Loves to Code <&#x2764>."],
+		typeSpeed: 33,
+		loop: true,
+	});
+});
